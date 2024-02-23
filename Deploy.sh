cd8d1d04aee5311f5a5e86970b48352aa003284f
thijs <thijsvanmarle@gmail.com>
2024-02-23 15:23:09 +0100

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -1,20 +1,19 @@
 #/bin/bash
 
 echo "installing Docker"
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh ./get-docker.sh --dry-run
+curl -sSL https://get.docker.com/ | sh
 
 echo "installing docker compose"
 curl -SL https://github.com/docker/compose/releases/download/v2.24.6/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 
 echo "Cloning API"
-git clone https:/github.com/spinningplayer/VeryBerryAPI.git
+git clone https://github.com/spinningplayer/VeryBerryAPI.git
 echo "Cloning Website"
-git clone https:/github.com/spinningplayer/VeryBerryWebsite.git
+git clone https://github.com/spinningplayer/VeryBerryRecordsWebsite.git
 echo "Building Docker images"
 docker build -t vbr-api VeryBerryAPI/
-docker build -t vbr-web VeryBerryWebsite/
+docker build -t vbr-web VeryBerryRecordsWebsite/
 echo "starting containers"
-docker-compose up
+docker-compose up -d
 echo "Populating web_data"
-docker cp VeryBerryWebsite/VBR-app/src/assets/* vbr-web:/usr/share/nginx/html/assets/
+docker cp VeryBerryRecordsWebsite/VBR-app/src/assets/* vbr-web:/usr/share/nginx/html/assets/
