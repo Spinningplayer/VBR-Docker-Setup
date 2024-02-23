656fc1b8e9406afa93663d353b0e2f1747e0c214
thijs <thijsvanmarle@gmail.com>
2024-02-23 14:55:52 +0100

    Update Deploy.sh

--- Deploy.sh
+++ Deploy.sh
@@ -2,7 +2,7 @@
 
 echo "installing Docker"
 curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh ./get-docker.sh --dry-run
+sh ./get-docker.sh --dry-run
 
 echo "installing docker compose"
 curl -SL https://github.com/docker/compose/releases/download/v2.24.6/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
